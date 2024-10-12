40604e4faadb976f7f74dbe231447d31d17eb428
qianpengzhan <873373549@qq.com>
2024-10-12 18:07:39 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,20 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: defaultTheme(),
-
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '钱鹏展随笔',
+  description: '钱鹏展随笔',
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: 'Java',
+        link: '/md/java/'
+      }
+    ],
+  }),
 })
