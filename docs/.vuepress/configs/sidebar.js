fe59869b9bc43d322bdedef05ca6cd78b503b174
钱鹏展 <873373549@qq.com>
2025-07-17 13:05:24 +0800

    提交

--- docs/.vuepress/configs/sidebar.js
+++ docs/.vuepress/configs/sidebar.js
@@ -136,7 +136,7 @@ export const sidebar =
                 },
                 {
                     text: '第七章: Spring Reactive Security 认证鉴权',
-                    link: '/md/note/reactive/reactive_006.md',
+                    link: '/md/note/reactive/reactive_007.md',
                     collapsible: true
                 }
             ]
