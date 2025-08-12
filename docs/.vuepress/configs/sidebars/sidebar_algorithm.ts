cc343ca60c4b38daab7208026f5505b7e96648c2
钱鹏展 <873373549@qq.com>
2025-08-12 13:47:15 +0800

    submit

--- docs/.vuepress/configs/sidebars/sidebar_algorithm.ts
+++ docs/.vuepress/configs/sidebars/sidebar_algorithm.ts
@@ -3,5 +3,10 @@ export const algorithmSidebar = [
         text: '算法 - 算法和数据结构',
         link: '/md/algorithm/algorithm_001.md',
         collapsible: true
+    },
+    {
+        text: '算法 - 认识复杂度',
+        link: '/md/algorithm/algorithm_002.md',
+        collapsible: true
     }
 ]
